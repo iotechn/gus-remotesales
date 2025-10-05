06145f08935c15db57232cf80b876f16f7e0321a
william.wei <william.wei@yimlink.com>
2025-10-05 21:16:43 +0800

    fix: url

--- src/main/java/com/dobbinsoft/gus/remotesales/controller/open/OpenWechatAuthController.java
+++ src/main/java/com/dobbinsoft/gus/remotesales/controller/open/OpenWechatAuthController.java
@@ -13,10 +13,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("/open/wechat-auth")
+@RestController
+@RequestMapping("/open/wechat-auth")
 public class OpenWechatAuthController {
 
     @Autowired
--- src/main/java/com/dobbinsoft/gus/remotesales/controller/open/OpenWecomAuthController.java
+++ src/main/java/com/dobbinsoft/gus/remotesales/controller/open/OpenWecomAuthController.java
@@ -10,10 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("/open/wecom-auth")
+@RestController
+@RequestMapping("/open/wecom-auth")
 public class OpenWecomAuthController {
 
     @Autowired
